ae8d07b0a9b09612b407c26ee4a4db48073a0a18
Maxac153 <maxac153@gmail.com>
2022-06-02 21:51:36 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./App.css";
+import Book from "./BookItem";
+import Logo from "./img/book.png";
+
+const App = () => {
+  const [author, setAuthor] = useState("");
+  const [name, setName] = useState("");
+
+  //Добавление книги
+  const handle = () => {
+    localStorage.setItem(`${author}${name}`, [author, name]);
+    setBooks([Object.values(localStorage)]);
+  };
+
+  const [books, setBooks] = useState([Object.values(localStorage)]);
+
+  return (
+    <div className="App">
+      <h1 className="title">Добавить книгу</h1>
+      <input
+        type="file"
+        id="myFileInput"
+        onChange={handleChange}
+      ></input>
+      <div className="add-book">
+        <img src={Logo} alt="Logo" className="logo"></img>
+        <div>
+          <input
+            className="input"
+            placeholder="Автор книги"
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <input
+            className="input"
+            placeholder="Название книги"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button className="btn" onClick={handle}>
+            Добавить книгу
+          </button>
+        </div>
+      </div>
+
+      {Object.values(localStorage).map((book, index) => (
+        <Book
+          key={index}
+          setBooks={setBooks}
+          author={book.split(",")[0]}
+          name={book.split(",")[1]}
+        ></Book>
+      ))}
+    </div>
+  );
+};
+export default App;
---
+++ src/BookItem.jsx
@@ -0,0 +1,18 @@
+const BookItem = ({setBooks, author, name }) => {
+  const remove = (id) => {
+    localStorage.removeItem(id);
+    setBooks([Object.values(localStorage)])
+  };
+
+  return (
+    <div className="item">
+      <p>Автор книги: <b>{author}</b></p>
+      <p> Название книги: <b>{name}</b></p>
+      <button id={author+name} className="btn" onClick={(e) => remove(e.target.id)}>
+      Удалить книгу
+    </button>
+    </div>
+  );
+};
+
+export default BookItem;
