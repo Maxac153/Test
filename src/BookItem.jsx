58950a453de590241e42a738f2dc880b195d6791
Maxac153 <maxac153@gmail.com>
2022-06-03 12:42:17 +0500

    Styles add

--- src/BookItem.jsx
+++ src/BookItem.jsx
@@ -1,18 +1,29 @@
-const BookItem = ({img, setBooks, author, name }) => {
+const BookItem = ({ img, setBooks, author, name }) => {
   //Удаление книги
   const remove = (id) => {
     localStorage.removeItem(id);
-    setBooks([Object.values(localStorage)])
+    setBooks([Object.values(localStorage)]);
   };
 
   return (
     <div className="item">
       <img className="img_book" src={img} alt="book" />
-      <p>Автор книги: <b>{author}</b></p>
-      <p> Название книги: <b>{name}</b></p>
-      <button id={author+name} className="btn" onClick={(e) => remove(e.target.id)}>
-      Удалить книгу
-    </button>
+      <div>
+        <p>
+          Автор книги: <b>{author}</b>
+        </p>
+        <p>
+          {" "}
+          Название книги: <b>{name}</b>
+        </p>
+      </div>
+      <button
+        id={author + name}
+        className="btn"
+        onClick={(e) => remove(e.target.id)}
+      >
+        Удалить книгу
+      </button>
     </div>
   );
 };
