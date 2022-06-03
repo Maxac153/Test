8cca710643b6461e68332be328a33989999125fd
Maxac153 <maxac153@gmail.com>
2022-06-03 12:39:28 +0500

    Size img

--- src/BookItem.jsx
+++ src/BookItem.jsx
@@ -7,7 +7,7 @@ const BookItem = ({img, setBooks, author, name }) => {
 
   return (
     <div className="item">
-      <img src={img} alt="book" />
+      <img className="img_book" src={img} alt="book" />
       <p>Автор книги: <b>{author}</b></p>
       <p> Название книги: <b>{name}</b></p>
       <button id={author+name} className="btn" onClick={(e) => remove(e.target.id)}>
